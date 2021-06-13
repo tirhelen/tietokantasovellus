ba860bf8f96e4a276840509a6e14627d3a5410ba
tirhelen <79860695+tirhelen@users.noreply.github.com>
2021-06-13 14:19:00 +0300

    Update countries.py

--- countries.py
+++ countries.py
@@ -1,9 +1,48 @@
 from database import database
 
 def get_list():
-    sql = "SELECT name FROM countries ORDER BY name"
-    result = database.session.execute(sql)
-    list = [i[0] for i in result.fetchall()]
+    #sql = "SELECT name FROM countries ORDER BY name"
+    #result = database.session.execute(sql)
+    #list = [i[0] for i in result.fetchall()]
+    #return list
+    list = ["Albania",
+            "Australia",
+            "Azerbaijan",
+            "Belgia",
+            "Bulgaria",
+            "Espanja",
+            "Georgia",
+            "Irlanti",
+            "Islanti",
+            "Iso-Britannia",
+            "Israel",
+            "Italia",
+            "Itävalta",
+            "Kreikka",
+            "Kroatia",
+            "Kypros",
+            "Latvia",
+            "Liettua",
+            "Malta",
+            "Moldova",
+            "Norja",
+            "Pohjois-Makedonia",
+            "Portugali",
+            "Puola",
+            "Ranska",
+            "Romania",
+            "Ruotsi",
+            "Saksa",
+            "San Marino",
+            "Serbia",
+            "Slovenia",
+            "Suomi",
+            "Sveitsi",
+            "Tanska",
+            "Tsekki",
+            "Ukraina",
+            "Venäjä",
+            "Viro"]
     return list
 
 def song_and_singer(country_id):

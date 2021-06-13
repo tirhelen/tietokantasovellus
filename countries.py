ca3755ad814b3e0e17f4403e7e861a43ff1f73c8
tirhelen <79860695+tirhelen@users.noreply.github.com>
2021-06-13 14:08:38 +0300

    Update countries.py

--- countries.py
+++ countries.py
@@ -3,7 +3,8 @@ from database import database
 def get_list():
     sql = "SELECT name FROM countries ORDER BY name"
     result = database.session.execute(sql)
-    return result.fetchall()
+    list = [i[0] for i in result.fetchall()]
+    return list
 
 def song_and_singer(country_id):
     sql = "SELECT singer, song FROM Songs_2021 WHERE country_id=:country_id"
